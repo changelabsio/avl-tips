9dae89a8cf0841d447813368ba5e875780832363
Robert Bo Davis <bo@interrobang.consulting>
2020-03-20 17:19:07 -0400

    UC env vars

--- packages/client/src/Config.js
+++ packages/client/src/Config.js
@@ -1,24 +1,24 @@
 
 export default {
-    city: process.env.city || "Asheville",
-    state: process.env.state || false,
-    mailto: process.env.mailto || "inquiries@avl.tips",
+    city: process.env.REACT_APP_CITY || "Asheville",
+    state: process.env.REACT_APP_STATE || false,
+    mailto: process.env.REACT_APP_MAILTO || "inquiries@avl.tips",
     form: {
-        url: process.env.form_url || "https://docs.google.com/forms/d/e/1FAIpQLSeVXG4EkNvg4iyAeYzRz45yiiadNh_OYZ9MG9moS4acJm_OFA/viewform"
+        url: process.env.REACT_APP_FORM_URL || "https://docs.google.com/forms/d/e/1FAIpQLSeVXG4EkNvg4iyAeYzRz45yiiadNh_OYZ9MG9moS4acJm_OFA/viewform"
     },
     image: {
-        src: process.env.image_src || "/avl.jpg",
+        src: process.env.REACT_APP_IMAGE_SRC || "/avl.jpg",
         attr: {
-            name: process.env.image_attr_name || "mogmismo",
-            href: process.env.image_attr_link || "https://www.flickr.com/photos/mogmismo/",
+            name: process.env.REACT_APP_IMAGE_ATTR_NAME || "mogmismo",
+            href: process.env.REACT_APP_IMAGE_ATTR_LINK || "https://www.flickr.com/photos/mogmismo/",
         }
     },
     api: {
         dev: {
-            url: process.env.api_dev_url || "https://65y0v85nve.execute-api.us-east-1.amazonaws.com/qa",
+            url: process.env.REACT_APP_API_DEV_URL || "https://65y0v85nve.execute-api.us-east-1.amazonaws.com/qa",
         },
         prod: {
-            url: process.env.api_prod_url || "https://yqzgbw1s1g.execute-api.us-east-1.amazonaws.com/dev"
+            url: process.env.REACT_APP_API_PROD_URL || "https://yqzgbw1s1g.execute-api.us-east-1.amazonaws.com/dev"
         }
     }
 };
